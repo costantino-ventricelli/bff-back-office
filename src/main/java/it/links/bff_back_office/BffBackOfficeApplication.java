401a6b615df603f07318c258afca079622463f6d
Luigi Albino <luigialbino@hotmail.it>
2025-04-09 11:45:47 +0200

    importo progetto

---
+++ src/main/java/it/links/bff_back_office/BffBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package it.links.bff_back_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffBackOfficeApplication.class, args);
+	}
+
+}
